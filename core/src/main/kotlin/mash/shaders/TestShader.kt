e19b9e86cc66bf3fca6e0d2fe90f2f143c50b233
Tommie Nygren <tommie.nygren@gmail.com>
2023-05-01 13:52:29 +0200

    Added a custom shader class for experimentation

---
+++ core/src/main/kotlin/mash/shaders/TestShader.kt
@@ -0,0 +1,36 @@
+package mash.shaders
+
+import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.g3d.Renderable
+import com.badlogic.gdx.graphics.g3d.Shader
+import com.badlogic.gdx.graphics.g3d.utils.RenderContext
+
+class TestShader : Shader {
+    override fun dispose() {
+        TODO("Not yet implemented")
+    }
+
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+    override fun compareTo(other: Shader?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun canRender(instance: Renderable?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun begin(camera: Camera?, context: RenderContext?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun render(renderable: Renderable?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun end() {
+        TODO("Not yet implemented")
+    }
+}
